e47b1bfeb07362fcc9f00aa3a3cac4124763be27
Michael Ledin <m.ledin@appheads.ru>
2017-11-13 16:31:48 +0300

    fix(*): remove redundant check.

--- pluginLoader.js
+++ pluginLoader.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = options =>
-  require('flavors-plugin-loader')(
-    typeof options.command === 'object' ? options.command : { command: options.command },
-    [require('flavors-plugin-loader-config')(options), require('flavors-plugin-loader-command')]);
+  require('flavors-plugin-loader')(options.command, [
+    require('flavors-plugin-loader-config')(options),
+    require('flavors-plugin-loader-command')
+  ]);
